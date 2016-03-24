c570ee00eef881febe44c66f677e079839029b92
Tim van Dijk <tim.vandijk96@gmail.com>
2016-03-25 00:15:27 +0100

    Made lists of all header names per category

---
+++ proj1_framework/webhttp/consts.py
@@ -0,0 +1,12 @@
+generalheaders = ["Cache-Control", "Connection", "Date", "Pragma", "Trailer", "Transfer-encoding",
+ "Upgrade", "Via", "Warning"]
+
+requestheaders = ["Accept", "Accept-Charset", "Accept-Encoding", "Accept-Language", "Authorization",
+ "Expect", "From", "Host", "If-Match", "If-Modified-Since", "If-None-Match", "If-Range",
+ "If-Unmodified-Since", "Max-Forwards", "Proxy-Authorization", "Range", "Referer", "TE", "User-Agent"]
+
+responseheaders = ["Accept-Ranges", "Age", "ETag", "Location", "Proxy-Authenticate", "Retry-After",
+"Server", "Vary", "WWW-Authenticate"]
+
+entityheaders = ["Allow", "Content-Encoding", "Content-Language", "Content-Length", "Content-Location",
+"Content-MD5", "Content-Range", "Content-Type", "Expires", "Last-Modified"]
